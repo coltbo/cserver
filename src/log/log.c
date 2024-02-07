fa1319a75190520b994d9e20750a4c31b653912f
Colten Begle <coltenbegle@icloud.com>
2024-02-06 22:04:56 -0500

    Added timestamps to logs. Also added HEAD

--- src/log/log.c
+++ src/log/log.c
@@ -1,53 +1,63 @@
+#include "log.h"
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "log.h"
+#include <time.h>
+
+#define MAX_TIME_STAMP 30
+#define MAX_LOG_SIZE 256
 
 static struct LoggerConfig logger = {.level = Warning};
 
-void logger_init(struct LoggerConfig config) {
-  logger = config;
-}
+void logger_init(struct LoggerConfig config) { logger = config; }
 
 bool should_log(enum LogLevel level) {
   return level >= logger.level ? true : false;
 }
 
-char *format_input_str(char *fmt, va_list args) {
-  char *buffer = malloc(sizeof(char *) * 250);
-  if (buffer != NULL) {
-    int rc = vsprintf(buffer, fmt, args);
-  }
+/* produces a timestamp in the format [dd mm yyyy hh:mm:ss] */
+void get_current_timestamp(char *timestamp, size_t len) {
+  time_t rawtime;
+  struct tm *timeinfo;
 
-  return buffer;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  snprintf(timestamp, len, "[%d/%d/%d %d:%d:%d]", timeinfo->tm_mday,
+           timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour,
+           timeinfo->tm_min, timeinfo->tm_sec);
 }
 
 void logmsg(enum LogLevel level, char *fmt, va_list args) {
-  char *buffer = format_input_str(fmt, args);
-  if (buffer == NULL)
+  char buffer[MAX_LOG_SIZE] = {0};
+  int bytes = vsnprintf(buffer, MAX_LOG_SIZE, fmt, args);
+  if (bytes <= 0) {
+    printf("[error] failed to create log message\n");
     return;
+  }
+
+  char timestamp[MAX_TIME_STAMP] = {0};
+  get_current_timestamp(timestamp, MAX_TIME_STAMP);
 
   switch (level) {
   case Debug:
-    printf("[debg] %s", buffer);
+    printf("[debg] %s %s", timestamp, buffer);
     break;
   case Information:
-    printf("[info] %s", buffer);
+    printf("[info] %s %s", timestamp, buffer);
     break;
   case Warning:
-    printf("[warn] %s", buffer);
+    printf("[warn] %s %s", timestamp, buffer);
     break;
   case Error:
-    printf("[error] %s", buffer);
+    printf("[error] %s %s", timestamp, buffer);
     break;
   default:
-    printf("[warn] %s", buffer);
+    printf("[warn] %s %s", timestamp, buffer);
     break;
   }
-
-  free(buffer);
 }
 
 void logger_log(enum LogLevel level, char *fmt, ...) {
--- src/server.c
+++ src/server.c
@@ -3,6 +3,7 @@
 #include "http/http.h"
 #include "lex/scan.h"
 #include "lex/token.h"
+#include "log/log.h"
 #include <netinet/in.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -16,6 +17,7 @@
 #include <unistd.h>
 
 int serverfd;
+toml_datum_t webroot;
 struct Config *config = NULL;
 
 void handler(int signo, siginfo_t *info, void *context) {
@@ -41,7 +43,145 @@ toml_datum_t get_file_mime_type(char *path) {
   return value;
 }
 
-void handle_request(int clientfd);
+void server_handle_get(char *uri, struct HttpResponse *res) {
+  int uri_len = strlen(webroot.u.s) + strlen(uri) + 1;
+
+  char req_path[uri_len];
+  snprintf(req_path, uri_len, "%s%s", webroot.u.s, uri);
+
+  // add file contents to response body
+  int rc = http_response_file_to_body(res, req_path);
+  if (rc == 0) {
+    logger_log(Debug, "read %ld bytes from %s\n", res->body_len, req_path);
+    
+    // add Content-Type header to response
+    toml_datum_t mime = get_file_mime_type(req_path);
+    if (mime.ok) {
+      if (http_response_add_header(res, "Content-Type", mime.u.s) != 0) {
+        logger_log(Error, "failed to add 'Content-Type' header\n");
+      }
+    } else {
+      if (http_response_add_header(res, "Content-Type",
+                                   "application/octet-stream") != 0) {
+        logger_log(Error, "failed to add 'Content-Type' header\n");
+      }
+    }
+
+    // add Content-Length header
+    char len_header[50];
+    snprintf(len_header, 50, "%ld",
+             res->body_len); // TODO - should probably handle this better
+    if (http_response_add_header(res, "Content-Length", len_header) != 0) {
+      logger_log(Error, "failed to add 'Content-Length' header\n");
+    }
+    res->status = OK;
+    res->status_msg = "OK";
+  } else if (rc == 1) {
+    logger_log(Error, "could not open requested file\n");
+    res->status = SERVER_ERROR;
+    res->status_msg = "Internal Server Error";
+  } else if (rc == 2) {
+    logger_log(Error, "file not found %s\n", req_path);
+    res->status = NOT_FOUND;
+    res->status_msg = "Not Found";
+  }
+}
+
+void server_handle_head(char *uri, struct HttpResponse *res) {
+  int uri_len = strlen(webroot.u.s) + strlen(uri) + 1;
+
+  char req_path[uri_len];
+  snprintf(req_path, uri_len, "%s%s", webroot.u.s, uri);
+
+  if (access(req_path, R_OK) != 0) {
+    logger_log(Error, "could not open requested file\n");
+    res->status = SERVER_ERROR;
+    res->status_msg = "Internal Server Error";
+    return;
+  }
+
+  FILE *stream;
+  if ((stream = fopen(
+		      req_path, "rb")) != NULL) {
+    // get file size to determine response buff size
+    fseek(stream, 0, SEEK_END);
+    long cont_len = ftell(stream);
+    fseek(stream, 0, SEEK_SET);
+
+    // add Content-Length header
+    char len_header[50];
+    snprintf(len_header, 50, "%ld", cont_len); // TODO - should probably handle this better
+    if (http_response_add_header(res, "Content-Length", len_header) != 0) {
+      logger_log(Error, "failed to add 'Content-Length' header\n");
+    }
+    res->status = OK;
+    res->status_msg = "OK";
+  } else {
+    logger_log(Error, "file not found %s\n", req_path);
+    res->status = NOT_FOUND;
+    res->status_msg = "Not Found";    
+  }
+}
+
+void handle_request(int clientfd) {
+  char request[MAX_REQUEST] = {0};
+  ssize_t valread = read(clientfd, request, MAX_REQUEST);
+  struct TokenArray *tarray = scan(request);
+  struct HttpResponse *res = http_response_alloc();
+
+  char *resbuf;
+  struct Token *method_token = find_token_by_type(tarray, METHOD);
+  struct Token *uri_token = find_token_by_type(tarray, URI);
+  struct Token *version_token = find_token_by_type(tarray, VERSION);
+
+  if (method_token == NULL || uri_token == NULL || version_token == NULL) {
+    char *msg;
+    if (method_token == NULL)
+      msg = "no method passed to request... rejecting\n";
+    if (uri_token == NULL)
+      msg = "no uri passed to request... rejecting\n";
+    if (version_token == NULL)
+      msg = "no version passed to request... rejecting\n";
+
+    logger_log(Error, "%s", msg);
+
+    res->status = BAD_REQUEST;
+    res->status_msg = "Bad Request";
+    size_t len = http_response_to_str(res, &resbuf);
+    send(clientfd, resbuf, strlen(resbuf), 0);
+  } else if (!is_http_version_compat(version_token->lexeme)) {
+    logger_log(Error, "unsupported http version\n");
+    res->status = NOT_SUPPORTED;
+    res->status_msg = "HTTP Version Not Supported";
+    size_t len = http_response_to_str(res, &resbuf);
+    send(clientfd, resbuf, strlen(resbuf), 0);
+  } else if (!is_method_supported(method_token->lexeme)) {
+    logger_log(Error, "unsuported http method... rejecting\n");
+    res->status = NOT_ALLOWED;
+    res->status_msg = "Method Not Allowed";
+    size_t len = http_response_to_str(res, &resbuf);
+    send(clientfd, resbuf, strlen(resbuf), 0);
+  } else {
+    logger_log(Information, "handling %s request for uri %s\n",
+               method_token->lexeme, uri_token->lexeme);
+
+    char *uri = (strcmp(uri_token->lexeme, "/") == 0) ? "/index.html"
+                                                      : uri_token->lexeme;
+
+    server_handle_get(uri, res);
+
+    size_t len = http_response_to_str(res, &resbuf);
+    send(clientfd, resbuf, len, 0);
+  }
+
+  if (resbuf != NULL) {
+    free(resbuf);
+    resbuf = NULL;
+  }
+  http_response_free(res);
+  free_token_array(tarray);
+  close(clientfd);
+}
 
 int server_run(int port) {
   // get configuration
@@ -49,8 +189,17 @@ int server_run(int port) {
   config = config_alloc(path);
 
   // initialize logger
-  struct LoggerConfig logger = { .level = config_get_log_level(config) };
+  struct LoggerConfig logger = {.level = config_get_log_level(config)};
   logger_init(logger);
+
+  // initialize base web dir
+  webroot = toml_string_in(
+      config->server, "webroot"); // TODO - have some way to check for nulls
+  if (!webroot.ok) {
+    webroot.u.s = "/home"; // TODO - set some reasonable default
+  }
+
+  logger_log(Information, "serving static files from '%s'\n", webroot.u.s);
 
   struct sockaddr_in address;
   socklen_t addrlen = sizeof(address);
@@ -100,104 +249,6 @@ int server_run(int port) {
     handle_request(clientfd);
   }
 
+  free(webroot.u.s);
   config_free(config);
-}
-
-void handle_request(int clientfd) {
-  char request[MAX_REQUEST] = {0};
-  ssize_t valread = read(clientfd, request, MAX_REQUEST);
-  struct TokenArray *tarray = scan(request);
-  struct HttpResponse *res = http_response_alloc();
-
-  char *resbuf;
-  struct Token *method_token = find_token_by_type(tarray, METHOD);
-  struct Token *uri_token = find_token_by_type(tarray, URI);
-  struct Token *version_token = find_token_by_type(tarray, VERSION);
-
-  if (method_token == NULL || uri_token == NULL || version_token == NULL) {
-    char *msg;
-    if (method_token == NULL)
-      msg = "no method passed to request... rejecting\n";
-    if (uri_token == NULL)
-      msg = "no uri passed to request... rejecting\n";
-    if (version_token == NULL)
-      msg = "no version passed to request... rejecting\n";
-
-    logger_log(Error, "%s", msg);
-
-    res->status = BAD_REQUEST;
-    res->status_msg = "Bad Request";
-    size_t len = http_response_to_str(res, &resbuf);
-    send(clientfd, resbuf, strlen(resbuf), 0);
-  } else if (!is_http_version_compat(version_token->lexeme)) {
-    logger_log(Error, "unsupported http version\n");
-    res->status = NOT_SUPPORTED;
-    res->status_msg = "HTTP Version Not Supported";
-    size_t len = http_response_to_str(res, &resbuf);
-    send(clientfd, resbuf, strlen(resbuf), 0);
-  } else if (!is_method_supported(method_token->lexeme)) {
-    logger_log(Error, "unsuported http method... rejecting\n");
-    res->status = NOT_ALLOWED;
-    res->status_msg = "Method Not Allowed";
-    size_t len = http_response_to_str(res, &resbuf);
-    send(clientfd, resbuf, strlen(resbuf), 0);
-  } else {
-    logger_log(Information, "handling %s request for uri %s\n", method_token->lexeme,
-           uri_token->lexeme);
-
-    toml_datum_t webroot = toml_string_in(config->server, "webroot");
-    if (!webroot.ok) {
-      webroot.u.s = "/home";
-    }
-
-    logger_log(Information, "serving static files from '%s'\n", webroot.u.s);
-
-    char *uri = (strcmp(uri_token->lexeme, "/") == 0) ? "/index.html"
-                                                      : uri_token->lexeme;
-    int uri_len = strlen(webroot.u.s) + strlen(uri) + 1;
-
-    char req_path[uri_len];
-    snprintf(req_path, uri_len, "%s%s", webroot.u.s, uri);
-
-    free(webroot.u.s);
-    webroot.u.s = NULL;
-
-    // add file contents to response body
-    int rc = http_response_file_to_body(res, req_path);
-    if (rc == 0) {
-      // add Content-Type header to response
-      toml_datum_t mime = get_file_mime_type(req_path);
-      if (mime.ok) {
-        if (http_response_add_header(res, "Content-Type", mime.u.s) != 0) {
-          logger_log(Error, "failed to add 'Content-Type' header\n");
-        }
-      } else {
-        if (http_response_add_header(res, "Content-Type",
-                                     "application/octet-stream") != 0) {
-          logger_log(Error, "failed to add 'Content-Type' header\n");
-        }
-      }
-      res->status = OK;
-      res->status_msg = "OK";
-    } else if (rc == 1) {
-      logger_log(Error, "could not open requested file\n");
-      res->status = SERVER_ERROR;
-      res->status_msg = "Internal Server Error";
-    } else if (rc == 2) {
-      logger_log(Error, "file not found %s\n", req_path);
-      res->status = NOT_FOUND;
-      res->status_msg = "Not Found";
-    }
-
-    size_t len = http_response_to_str(res, &resbuf);
-    send(clientfd, resbuf, len, 0);
-  }
-
-  if (resbuf != NULL) {
-    free(resbuf);
-    resbuf = NULL;
-  }
-  http_response_free(res);
-  free_token_array(tarray);
-  close(clientfd);
 }
